16da9e56dd8489ac0d51b0cf0873040030415249
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2024-02-07 19:20:33 -0400

    chore: adding import in app module

--- library-app/src/app/app.module.ts
+++ library-app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AutorListComponent } from './autorComponents/autor-list/autor-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ClientListComponent } from './clientComponents/client-list/client-list.component';
+import { BookListComponent } from './bookComponents/book-list/book-list.component';
+import { LoanListComponent } from './loanComponents/loan-list/loan-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutorListComponent,
+    ClientListComponent,
+    BookListComponent,
+    LoanListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
